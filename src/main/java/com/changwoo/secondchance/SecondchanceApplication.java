b566d503e70f85e893e84aa457c004a6d5d31ace
ShimChang <95128916+ShimChang@users.noreply.github.com>
2023-11-29 20:11:32 +0900

    first commit

---
+++ src/main/java/com/changwoo/secondchance/SecondchanceApplication.java
@@ -0,0 +1,13 @@
+package com.changwoo.secondchance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondchanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondchanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changwoo/secondchance/SecondchanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changwoo.secondchance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondchanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
